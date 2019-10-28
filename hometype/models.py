0a9a889cabfbf5eabf327293d5e3bc2a21fe8fec
Yonas Negash <yonas.negash91@gmail.com>
2019-10-28 19:07:52 +0300

    create migrations and migrate models

--- hometype/models.py
+++ hometype/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class HomeType(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    register_date = models.DateTimeField(default=datetime.now, blank=True)
+    def __str__(self):
+        return self.name
