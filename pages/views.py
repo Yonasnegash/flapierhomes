372fb325ca26785107fb3f11d8e34131b160e0e8
Yonas Negash <yonas.negash91@gmail.com>
2019-11-11 12:53:34 +0300

    modify contents of files in templates folder and in listings/views.py

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, bathroom_choices, house_type, city_choices, sqft_choices
+
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    featured_listing = Listing.objects.all().filter(is_featured=True)
+
+    context = {
+        'listings': listings,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'bathroom_choices': bathroom_choices,
+        'house_type': house_type,
+        'city_choices': city_choices,
+        'sqft_choices': sqft_choices,
+        'featured_listing': featured_listing
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
